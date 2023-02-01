940bb6bb8029fe136e488bdc15a81d9c803e6c52
sadmanador <sadman.ador@gmail.com>
2023-02-01 18:22:46 +0600

    hotels data are working

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require('cors');
-
+const cors = require("cors");
+const hotels = require("./data/hotels.json");
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-res.send('Hello world')
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.get("/hotels", (req, res) => {
+  res.send(hotels);
 });
 
 app.listen(port, () => {
-console.log(`listening port 5000`);
+  console.log(`listening port 5000`);
 });
+
+
