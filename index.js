8c7d05e8fc536d90317d1f3077568bf6e5355d63
sadmanador <sadman.ador@gmail.com>
2023-02-04 19:56:16 +0600

    server side script modified for location and id url

--- index.js
+++ index.js
@@ -16,25 +16,28 @@ app.get("/hotels", (req, res) => {
 
 app.get("/location/:id", (req, res) => {
   const id = req.params.id;
-  if(id > 3){
+  if (id > 3) {
     res.send(hotels);
-  }
-  else{
-    const locations = hotels.filter(hotel=> hotel.location_id == id);
+  } else {
+    const locations = hotels.filter((hotel) => hotel.location_id == id);
     res.send(locations);
   }
 });
 
-app.get("/id/:id",(req,res) => {
+app.get("/location", (req, res) => {
+  res.send(hotels);
+});
+
+app.get("/id/:id", (req, res) => {
   const id = req.params.id;
-  const hotel_id = hotels.find(hotel=> hotel.id == id);
+  const hotel_id = hotels.find((hotel) => hotel.id == id);
   res.send(hotel_id);
-},)
+});
 
-
+app.get("/id", (req, res) => {
+  res.send(hotels);
+});
 
 app.listen(port, () => {
   console.log(`listening port 5000`);
 });
-
-
