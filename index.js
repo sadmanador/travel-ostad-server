3634b4746e4ecd0f3ed99b539223e1c4c1dc84b9
sadmanador <sadman.ador@gmail.com>
2023-02-04 19:50:05 +0600

    api working based on id and location_id

--- index.js
+++ index.js
@@ -14,6 +14,25 @@ app.get("/hotels", (req, res) => {
   res.send(hotels);
 });
 
+app.get("/location/:id", (req, res) => {
+  const id = req.params.id;
+  if(id > 3){
+    res.send(hotels);
+  }
+  else{
+    const locations = hotels.filter(hotel=> hotel.location_id == id);
+    res.send(locations);
+  }
+});
+
+app.get("/id/:id",(req,res) => {
+  const id = req.params.id;
+  const hotel_id = hotels.find(hotel=> hotel.id == id);
+  res.send(hotel_id);
+},)
+
+
+
 app.listen(port, () => {
   console.log(`listening port 5000`);
 });
